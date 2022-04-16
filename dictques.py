0ea4ccb8938b7a8146a728b67b8b957dc186f2ca
DevakXD <101389277+DevakProprogrammer@users.noreply.github.com>
2022-04-16 13:36:27 +0530

    Add files via upload

---
+++ dictques.py
@@ -0,0 +1,13 @@
+# Exersize - 1
+# Question - Make a dictionary which takes input from user and Tells Meaning of word>
+# Answer->
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Welcoming User
+print("welcome to dictionary\nAsk Any word you want?")
+
+dictionary = {"Abase":"To cause TO feel shame |to know more -'https://www.merriam-webster.com/dictionary/abase'", "Callow":"Young And unexperienced |to know more -'https://www.merriam-webster.com/dictionary/callow'", "endear":"make attractive |to know more -'https://www.merriam-webster.com/dictionary/endear'", "dally":"waste time |to know more -'https://www.merriam-webster.com/dictionary/dally'"}
+
+AskWord = input("Which word's Meaning you want to know -> ")
+print(dictionary[AskWord])
+
+print("Thanks For using Dictionary!")
